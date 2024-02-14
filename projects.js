1432d5da9ff5b68e6d558810188c4da88900985c
chloeji8888 <chloeji8888@gmail.com>
2024-02-14 10:23:03 -0800

    updated

--- projects.js
+++ projects.js
@@ -1,10 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var containers = document.querySelectorAll('.content-container');
+    // Initialize the display of the content blocks
+    containers.forEach(function(container, index) {
+        var content = container.querySelector('.content');
+        if (index === 0) { // Automatically display the first content
+            content.style.display = 'block';
+            container.classList.add('active');
+        } else { // Hide other contents
+            content.style.display = 'none';
+        }
+    });
+
+    // Add click event listeners to the titles
     document.querySelectorAll('.title').forEach(function(title) {
         title.addEventListener('click', function () {
             var container = this.parentNode;
             var content = container.querySelector('.content');
+            // Check if the clicked title's content is already displayed
+            var curContent = container.querySelector('.curcontent');
             if (content.style.display === 'none') {
                 content.style.display = 'block';
+                container.classList.add('active');
+            } else if (curContent && content !== curContent) {
+                // If there's a .curcontent and it's different from the clicked one, show it
+                curContent.style.display = 'block';
                 container.classList.add('active');
             } else {
                 content.style.display = 'none';
